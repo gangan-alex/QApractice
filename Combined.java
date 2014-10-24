4052b7ad79f86a5b60718581e4767260a2f1b117
agangan <alexander.gangan@dataart.com>
2014-10-24 14:36:11 +0300

    private-protected

--- Combined.java
+++ Combined.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
 
-public class Combined {
+protected class Combined {
 	JTextField displayField = new JTextField();
 	JButton buttonPoint = new JButton(".");
 	JButton buttonEqual = new JButton("=");
