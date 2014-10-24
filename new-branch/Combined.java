26b6a790483934dd9e8b3e648f9179db77dfb17f
agangan <alexander.gangan@dataart.com>
2014-10-24 12:51:36 +0300

    renamed panel

---
+++ new-branch/Combined.java
@@ -0,0 +1,70 @@
+package Simple;
+
+import javax.swing.*;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+
+public class Combined {
+	JTextField displayField = new JTextField();
+	JButton buttonPoint = new JButton(".");
+	JButton buttonEqual = new JButton("=");
+	JButton buttonPlus = new JButton("+");
+	JButton buttonDiv = new JButton("/");
+	JButton buttonMinus = new JButton("-");
+	JButton buttonMulti = new JButton("x");
+	JButton numButton[] = new JButton[10];
+
+	Combined() {
+		JPanel windowContent = new JPanel();
+		BorderLayout bl = new BorderLayout();
+		windowContent.setLayout(bl);
+		displayField.setHorizontalAlignment(JTextField.RIGHT);
+		windowContent.add("North", displayField);
+		JPanel PanelOne = new JPanel();
+
+		GridLayout NumGrid = new GridLayout(4, 3);
+		PanelOne.setLayout(NumGrid);
+		for (int i = 0; i < 10; i++) {
+			String digits = new Integer(i).toString();
+			numButton[i] = new JButton(digits);
+			PanelOne.add(numButton[i]);
+		}
+		PanelOne.add(buttonPoint);
+		PanelOne.add(buttonEqual);
+		windowContent.add("Center", PanelOne);
+
+		JPanel ops = new JPanel();
+		GridLayout ActionGrid = new GridLayout(4, 1);
+		ops.setLayout(ActionGrid);
+		ops.add(buttonPlus);
+		ops.add(buttonMinus);
+		ops.add(buttonMulti);
+		ops.add(buttonDiv);
+
+		windowContent.add("East", ops);
+		JFrame frame = new JFrame("Calculator");
+		frame.setContentPane(windowContent);
+
+		// making window size enough to show all the components
+		frame.pack();
+
+		// making the window visible
+		frame.setVisible(true);
+
+		Clicked calcEngine = new Clicked();
+		buttonPlus.addActionListener(calcEngine);
+		buttonMinus.addActionListener(calcEngine);
+		buttonMulti.addActionListener(calcEngine);
+		buttonDiv.addActionListener(calcEngine);
+
+		for (int i = 0; i < 10; i++) {
+		numButton[i].addActionListener(calcEngine);}
+
+		buttonPoint.addActionListener(calcEngine);
+		buttonEqual.addActionListener(calcEngine);
+	}
+
+	public static void main(String[] args) {
+		Combined calc = new Combined();
+	}
+}
